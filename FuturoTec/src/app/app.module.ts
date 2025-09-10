661c22cf413868607356eb1efb5a57900c8fa9f8
InfonetNatyDuGu <infonet689@gmail.com>
2025-09-10 11:03:10 -0300

    mudei umas coisa

--- FuturoTec/src/app/app.module.ts
+++ FuturoTec/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { CoursesSectionComponent } from './components/courses-section/courses-section.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
--- FuturoTec/src/app/home/home.page.ts
+++ FuturoTec/src/app/home/home.page.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CoursesSectionComponent } from '../components/courses-section/courses-section.component'; // Importa o componente criado
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    CoursesSectionComponent
+  ],
 })
 export class HomePage {
-
   constructor() {}
-
 }
